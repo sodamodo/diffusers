437b6bb2bd1c1190d6f937dc5e65c96fd3cc9960
Zack Noah <zacharybnoah@gmail.com>
2022-09-24 20:05:05 -0700

    Update safety_checker.py

--- src/diffusers/pipelines/stable_diffusion/safety_checker.py
+++ src/diffusers/pipelines/stable_diffusion/safety_checker.py
@@ -69,7 +69,8 @@ class StableDiffusionSafetyChecker(PreTrainedModel):
 
         for idx, has_nsfw_concept in enumerate(has_nsfw_concepts):
             if has_nsfw_concept:
-                images[idx] = np.zeros(images[idx].shape)  # black image
+                pass
+                #images[idx] = np.zeros(images[idx].shape)  # black image
 
         if any(has_nsfw_concepts):
             logger.warning(
